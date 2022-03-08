37a8bf68b9d76b28b7d450fb006a95e65671bee9
rbr <rbr@arcor.de>
2022-03-08 14:44:28 +0100

    change build behavior
    rename to name bchain
    extends version info with makefile

--- blockchain/cmd/version.go
+++ blockchain/cmd/version.go
@@ -6,6 +6,18 @@ import (
 	"github.com/spf13/cobra"
 )
 
+// var section
+var (
+	major     = "0"
+	minor     = "8"
+	patch     = "1"
+	buildTag  = "-"
+	buildDate = "-"
+	appName   = "bchain - blockchain structure playground"
+	author    = "raibru <github.com/raibru>"
+	license   = "MIT License (c) 2022 raibru"
+)
+
 func init() {
 	rootCmd.AddCommand(versionCmd)
 }
@@ -15,6 +27,9 @@ var versionCmd = &cobra.Command{
 	Short: "Print the version number of bc",
 	Long:  `The version of blockchain playground`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("bc blockchain playground v0.8.0")
+		fmt.Printf("%s - v%s.%s.%s\n", appName, major, minor, patch)
+		fmt.Printf("  Build-%s (%s)\n", buildTag, buildDate)
+		fmt.Printf("  author : %s\n", author)
+		fmt.Printf("  license: %s\n\n", license)
 	},
 }
